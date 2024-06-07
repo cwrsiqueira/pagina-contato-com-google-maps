97da396d321ef21498899f19392707256c05eb39
cwrsiqueira <cwrsiqueira@msn.com>
2024-06-07 03:42:44 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Empresa | Página de Contato</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container" style="height: 100vh;">
+        <div class="d-flex flex-column h-100 justify-content-center align-items-center">
+            <div class="row d-flex flex-column justify-content-center align-items-center mb-3">
+                <h1 class="text-primary-emphasis">Empresa</h1>
+            </div>
+            <div class="row shadow-lg w-75 h-50 rounded">
+                <div class="col-sm w-100 p-0 rounded">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.8171012122248!2d-51.11795582398702!3d-0.044790535531427755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x929e1cbdc284f1ed%3A0x6398c0b48671b866!2sPronorte%20Im%C3%B3veis%20Ltda.!5e0!3m2!1spt-BR!2sbr!4v1717738126393!5m2!1spt-BR!2sbr" class="w-100 h-100 rounded" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="col-sm p-3 d-flex flex-column justify-content-center align-items-center">
+                    <form action="" method="post" class="w-100">
+                        <div class="mb-3">
+                            <label for="" class="form-label">E-mail</label>
+                            <input type="text" name="" id="" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="" class="form-label">Assunto</label>
+                            <input type="text" name="" id="" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="" class="form-label">Mensagem</label>
+                            <textarea type="text" name="" id="" class="form-control"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <button type="submit" class="btn btn-primary">Enviar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
